2c6a321610735e479c736a01eba08939dfcb2217
YD1234564789 <a8111202003@gmail.com>
2023-04-28 02:43:43 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
 const port = 3000
+
+// 僅非正式環境使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 建立連線
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 // express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
